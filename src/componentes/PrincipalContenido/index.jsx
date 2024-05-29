dd04f2984e08b69943e9f4932dbd8ad9da447f1b
fmautinoh20 <fmautinoh20@gmail.com>
2024-05-29 15:56:06 -0500

    principal_falta_responsividad

--- src/componentes/PrincipalContenido/index.jsx
+++ src/componentes/PrincipalContenido/index.jsx
@@ -3,20 +3,20 @@ const PrincipalContenido = () => {
     <div className="absolute inset-0">
       <div className="absolute top-8 left-[17%] right-8 bottom-8 Contenido items-center">
         <img
-          className="relative top-[10%] left-[5%] w-40 h-40 shadow-inner rounded-full bg-cover"
+          className="relative top-[10%] left-[5%] w-40 h-40 shadow-inner rounded-full"
           src="src/assets/ASD-1.svg"
           alt="Placeholder Image"
         />
         <div className="justify-center">
           <div
-            className="absolute top-20 left-[12%] right-0 mx-auto w-3/4 text-center text-[#0092FC] text-4xl font-black"
-            style={{ fontFamily: "Alfa Slab One", lineHeight: "2" }}
+            className="absolute top-20 left-[12%] right-0 mx-auto w-3/4 text-center text-[#0092FC] text-4xl font-bold"
+            style={{ fontFamily: "Alfa Slab One", lineHeight: "1.4" }}
           >
             INSTITUCIÓN EDUCATIVA EMBLEMÁTICA “COLEGIO DE LA LIBERTAD”
           </div>
           <div
-            className="absolute top-80 left-[9%] right-0 mx-auto w-3/4 text-center text-[#568BDA] text-5xl font-bold"
-            style={{ fontFamily: "Amaranth", lineHeight: "1.9" }}
+            className="absolute top-80 left-[12%] right-0 mx-auto w-3/4 text-center text-[#568BDA] text-4xl font-bold"
+            style={{ fontFamily: "Amaranth", lineHeight: "1.4" }}
           >
             BIENVENIDO AL REPOSITORIO DE DOCUMENTOS ADMINISTRATIVOS
           </div>
